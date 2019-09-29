9c7b8a76616d5b85eff0d4f6c145712853625aa1
Midhun Kandathil <Midhun.Kandathil@brandmuscle.com>
2019-09-29 12:46:30 +0530

    Added angular material and imported basic material ui components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { NotesComponent } from './components/notes/notes.component';
+import {
+  MatIconModule,
+  MatButtonModule,
+  MatMenuModule,
+  MatInputModule,
+} from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    NotesComponent
+    NotesComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatIconModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
